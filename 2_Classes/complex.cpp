ff3801f3114f71cfd7b2c8b7fe8323ed1a33a1bd
Daniel Ingram <ingramds@appstate.edu>
2019-02-06 20:28:09 -0500

    Override operator+

--- 2_Classes/complex.cpp
+++ 2_Classes/complex.cpp
@@ -6,6 +6,8 @@ class complex
     friend const double& imag(const complex& c);
     friend complex operator+(const complex& c1, const complex& c2);
     friend complex operator+(const complex& c, double d);
+    friend complex operator*(const complex& c1, const complex& c2);
+    friend complex operator*(const complex& c, double d);
     
     public:
         complex(double r=0, double i=0) : r(r), i(i) {}
@@ -27,6 +29,16 @@ inline complex operator+(const complex& c, double d)
     return complex(c.r + d, c.i);
 }
 
+inline complex operator*(const complex& c1, const complex& c2)
+{
+    return complex(c1.r * c2.r - c1.i * c2.i, c1.r * c2.i + c1.i * c2.r);
+}
+
+inline complex operator*(const complex& c, double d)
+{
+    return complex(d * c.r, d * c.i);
+}
+
 std::ostream& operator<<(std::ostream& os, complex& c)
 {
     return os << real(c) << " + " << imag(c) << "i";
@@ -39,11 +51,15 @@ int main(int argc, char** argv)
     complex z3 = z1 + z2;
     complex z4 = z1 + 6.0;
     complex z5 = 7.0 + z1;
+    complex z6 = z1 * z2;
+    complex z7 = 2 * z1;
 
     std::cout << "z1 = " << z1 << std::endl;
     std::cout << "z2 = " << z2 << std::endl;
     std::cout << "z3 = " << z3 << std::endl;
     std::cout << "z4 = " << z4 << std::endl;
     std::cout << "z5 = " << z5 << std::endl;
+    std::cout << "z6 = " << z6 << std::endl;
+    std::cout << "z7 = " << z7 << std::endl;
     return 0;
 }
